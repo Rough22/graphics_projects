7ec812830f475de7cabc02571f48282208c9d382
Rough22 <44208637+Rough22@users.noreply.github.com>
2019-10-09 01:51:15 +0600

    Repository Created

---
+++ Clipping/PointClipping/cmake-build-debug/CMakeFiles/PointClipping.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PointClipping.dir/main.cpp.obj"
+  "PointClipping.exe"
+  "PointClipping.exe.manifest"
+  "PointClipping.pdb"
+  "libPointClipping.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PointClipping.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
